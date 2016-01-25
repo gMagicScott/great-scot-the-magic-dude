590def233eb3d46873366e9e087cc21bc1beeeb6
Scott Lesovic <scott@scottlesovic.com>
2016-01-24 20:54:33 -0500

    moar...

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,9 @@
 "use strict";
 
-var gulp        = require('gulp');
-var browserSync = require('browser-sync').create();
-var sass        = require('gulp-sass');
+var gulp         = require('gulp');
+var browserSync  = require('browser-sync').create();
+var sass         = require('gulp-sass');
+var autoprefixer = require('gulp-autoprefixer');
 
 // Static Server + watching scss/html files
 gulp.task('serve', ['sass'], function() {
@@ -19,6 +20,10 @@ gulp.task('serve', ['sass'], function() {
 gulp.task('sass', function() {
     return gulp.src("scss/*.scss")
         .pipe(sass())
+        .pipe(autoprefixer({
+            browsers: ['last 2 versions'],
+            cascade: false
+        }))
         .pipe(gulp.dest("./css"))
         .pipe(browserSync.stream());
 });
